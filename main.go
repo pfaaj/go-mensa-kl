391f10e014cbb4e2910b9651becab2b6eccbc902
Paulo Aragao <aragao@cs.uni-kl.de>
2019-10-18 17:36:27 +0200

    added some examples as reference for the termdash and some cleaning of the meals. still no buffet

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 
+	"strings"
+
 	"github.com/gocolly/colly"
 )
 
@@ -16,16 +18,40 @@ type MensaPlan struct {
 
 const agent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0"
 
+func filterNonempty(ss []string, clean bool) (ret []string) {
+	for _, s := range ss {
+		noempty := strings.Replace(s, " ", "", -1)
+		if len(noempty) >= 1 {
+			if clean == true {
+				ret = append(ret, noempty)
+			} else {
+				ret = append(ret, s)
+			}
+
+		}
+	}
+	return
+}
+
 func main() {
 	c := colly.NewCollector()
 
-	// Find and visit all links
+	// Find and meal links
 	c.OnHTML("div[class=dailyplan]", func(e *colly.HTMLElement) {
 		category := e.ChildText("div[class=c10l]")
 		title := e.ChildText("div[class=c90r]")
-		fmt.Println("Title: " + title)
-		fmt.Println("Category: " + category)
+		fmt.Printf("Meals: %q \n", filterNonempty(strings.Split(title, "\n"), false))
+		categories := filterNonempty(strings.Split(category, "\n"), true)
+		fmt.Printf("Categories: %q\n", categories)
 		fmt.Println("Date: " + e.ChildText("h5"))
+	})
+
+	c.OnHTML("div[class=buffet]", func(e *colly.HTMLElement) {
+		category := "Buffet"
+		title := e.ChildText("div[class=buffet-components]")
+		fmt.Println("Title: " + strings.TrimSpace(title))
+		fmt.Println("Category: " + category)
+		fmt.Println("Description: " + e.ChildText("h5"))
 	})
 
 	c.OnRequest(func(r *colly.Request) {
