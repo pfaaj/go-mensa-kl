183f8e1cb89c870ad2186f944ac87539f4b1e57b
Paulo A <aragao@cs.uni-kl.de>
2019-10-24 12:58:48 +0200

    downloading atrium meals but still not showing in the dashboard

--- mensa/mensa.go
+++ mensa/mensa.go
@@ -37,14 +37,24 @@ const agent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 
 
 func getMeals(e *colly.HTMLElement, plans *Plans) {
 	category := e.ChildText("div[class=c10l]")
+	if strings.TrimSpace(category) == "" {
+		category = "Atrium"
+	}
 	title := e.ChildText("div[class=c90r]")
 	var plan Plan
 	plan.Meals = filterNonempty(strings.Split(title, "\n"), false)
+	if category == "Atrium" {
+		fmt.Println("Atrium: ")
+		fmt.Println(plan.Meals)
+		plan.Categories = append(plan.Categories, category)
+	} else {
+		plan.Categories = filterNonempty(strings.Split(category, "\n"), true)
+	}
 	for i, meal := range plan.Meals {
 		meal = strings.Replace(meal, "Studenten", "\nStudenten", -1)
 		plan.Meals[i] = meal
 	}
-	plan.Categories = filterNonempty(strings.Split(category, "\n"), true)
+
 	plan.Date = e.ChildText("h5")
 	plans.AllMeals = append(plans.AllMeals, plan)
 }
@@ -76,12 +86,17 @@ func GetMensaPlan() (plans Plans) {
 		colly.CacheDir("./cache"),
 	)
 
-	//atrium := colly.NewCollector(
-	//	colly.CacheDir("./cache"),
-	//)
+	atrium := colly.NewCollector(
+		colly.CacheDir("./cache"),
+	)
 
-	// Find and meal links
+	// get all meals
 	c.OnHTML("div[class=dailyplan]", func(e *colly.HTMLElement) {
+		getMeals(e, &plans)
+	})
+
+	// get atriums meals
+	atrium.OnHTML("div[class=dailyplan]", func(e *colly.HTMLElement) {
 		getMeals(e, &plans)
 	})
 
@@ -120,7 +135,13 @@ func GetMensaPlan() (plans Plans) {
 		fmt.Println("Visited", r.URL)
 	})
 
+	atrium.OnRequest(func(r *colly.Request) {
+		r.Headers.Set("User-Agent", agent)
+		fmt.Println("Visited", r.URL)
+	})
+
 	c.Visit("https://www.studierendenwerk-kaiserslautern.de/kaiserslautern/essen-und-trinken/tu-kaiserslautern/mensa/")
+	atrium.Visit("https://www.studierendenwerk-kaiserslautern.de/kaiserslautern/essen-und-trinken/tu-kaiserslautern/mensaria-atrium/")
 
 	return
 
